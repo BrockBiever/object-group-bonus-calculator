64d6b8fe78e7eda577d98fea0ba17e665784b9f9
Brock Biever <brock.biever10@gmail.com>
2024-09-04 15:11:27 -0500

    live solve

--- client.js
+++ client.js
@@ -32,7 +32,7 @@ const employees = [
   }
 ];
 
-console.log('array of employee data: ',  employees );
+console.log('array of employee data: ', employees);
 
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
@@ -45,13 +45,88 @@ console.log('array of employee data: ',  employees );
 
 
 
+function calculateIndividualEmployeeBonus(employee) {
 
-// This function will calculate 1 employee's bonus!
-//
-function calculateIndividualEmployeeBonus( employee ) {  
-  // your logic here
-  
-  
-  // return new object with bonus results
+  console.log("calculateIndividualEmployeeBonus() was called with param: ", employee)
+
+  let bonusPercentage = 0
+  // * Convert annualSalary into a number, from string
+  const salaryNumber = Number(employee.annualSalary)
+
+  // Review Rating Rules
+
+  // If rating is <= 0, no change to bonus
+  if (employee.reviewRating <= 2) {
+    bonusPercentage = 0
+  }
+  // If rating == 3, bonus = 4
+  if (employee.reviewRating === 3) {
+    bonusPercentage = 4
+  }
+  // If rating is 4, bonus = 6
+  if (employee.reviewRating === 4) {
+    bonusPercentage = 6
+  }
+  // If rating is 5, bonus = 10
+  if (employee.reviewRating === 5) {
+    bonusPercentage = 10
+  }
+  // console.log("Bonus after rating review: ", bonusPercentage)
+
+
+  // Employee number rule
+  if (employee.employeeNumber.length === 4) {
+    bonusPercentage += 5
+  }
+  // console.log("Bonus after employee number review:", bonusPercentage)
+
+
+  // Rule If income is above 65k, minus 1%
+  if (salaryNumber > 65000) {
+    bonusPercentage -= 1
+  }
+  // console.log("Bonus after income review:", bonusPercentage)
+
+  // Rule for max an min of percentage
+  if (bonusPercentage >= 13) {
+    bonusPercentage = 13
+  }
+  if (bonusPercentage < 0) {
+    bonusPercentage = 0
+  }
+  // console.log("Bonus after min/max:", bonusPercentage)
+
+  // Calculate total bonus
+  // Convert percentage number into a decimal number
+  let bonusAsDecimal = bonusPercentage / 100
+  let totalBonus = Math.floor(salaryNumber * bonusAsDecimal)
+
+  // Calculate total compensation
+  let totalCompensation = salaryNumber + totalBonus
+
+
+  // Return an object with the following properties:
+  let result = {
+    name: employee.name,
+    bonusPercentage: bonusPercentage,
+    totalCompensations: totalCompensation,
+    totalBonus: totalBonus
+  }
+
+  return result
 
 }
+
+// ! TEST:
+// Test for 1 employee
+// let singleEmployeeTest = calculateIndividualEmployeeBonus(employees[2])
+// console.log("singleEmployeeTest: ", singleEmployeeTest)
+
+// Loop over the function, each instance of the loop will provide the next employee.
+function testAllEmployees() {
+  for (let employee of employees) {
+    calculateIndividualEmployeeBonus(employee)
+  }
+}
+
+testAllEmployees()
